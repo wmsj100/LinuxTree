0c4b1452a977c590cc60245497272abf25fb7687
wmsj100 <wmsj100@hotmail.com>
2017-09-27 00:23:04 +0800

    delete study_0923

--- java/headfirst/study_0923/myFirstApp.java
+++
@@ -1,5 +0,0 @@
-public class myFirstApp{
-	public static void main(String[], args){
-	System.out.print("hello world");
-}
-}
--- java/headfirst/study_0923/testIf.java
+++
@@ -1,8 +0,0 @@
-class testIf{
-	public static void main(String[], args){
-		int x = 3;
-		if(x == 3){
-			System.out.println("hello world");
-		}
-	}
-}
