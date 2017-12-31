0ce9f384ef6786737c9afbf24e21a41f0052cd77
wmsj100 <wmsj100@hotmail.com>
2018-01-01 00:10:24 +0800

    python 模块

---
+++ Python/Study/collectionsTest1.py
@@ -0,0 +1,10 @@
+from collections import Counter
+from collections import defaultdict
+
+s = [('yellow', 1),('blue', 2),('yellow', 3),('blue', 4),('red', 1)]
+d = defaultdict(list)
+for k,v in s:
+    d[k].append(v)
+
+val = d.items()
+print(val)
