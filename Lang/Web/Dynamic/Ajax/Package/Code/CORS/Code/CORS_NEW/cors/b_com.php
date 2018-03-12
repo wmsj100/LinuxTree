26b5112dc10049afbb15024593b073be70d97c39
wmsj100 <wmsj100@hotmail.com>
2018-03-13 00:25:04 +0800

    压缩web的html内容

--- Lang/Web/Dynamic/Ajax/Package/Code/CORS/Code/CORS_NEW/cors/b_com.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin:http://wmsj100.github.io");
-$info = '{"name": "wmsj", "age": 10}';
-echo $info;
--- Lang/Web/Dynamic/Ajax/Package/Code/CORS/Code/CORS_OLD_OLD/JSONP/b_JSONP.php
+++
@@ -1,4 +0,0 @@
-<?php
-$jsonpData = $_GET["callback"];
-$content = '{"item1": "#D34545", "item2": "#CCC83C", "item3": "#2DDF43", "item4": "#9628DD"}';
-echo $jsonpData."(".json_encode($content).")";
